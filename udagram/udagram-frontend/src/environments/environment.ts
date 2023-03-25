e82b44f133a11dab9e28aa29912eda3fcf3d1b6a
Tarek Elkanaria <tarekelkanaria@gmail.com>
2023-03-25 20:22:53 +0200

    Modify URL

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'udagram-api-dev2.us-east-2.elasticbeanstalk.com/api/v0',
+  apiHost: 'https://udagram-api-dev2.us-east-2.elasticbeanstalk.com/api/v0',
 };
 
 /*
